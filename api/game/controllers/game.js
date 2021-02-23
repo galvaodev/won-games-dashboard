df5a2db2fadeb2499484ae02dc8a85b3d3dd7f82
Macbook <galvaopp@gmail.com>
2021-02-22 22:02:48 -0300

    Feature: Finish scrapper API gog

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Stating to populate...");
+
+    const options = {
+      page: "1",
+      sort: "popularity",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
+
+    ctx.send("Finished population!");
+  },
+};
